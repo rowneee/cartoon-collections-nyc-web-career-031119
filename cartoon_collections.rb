cfb01edba6bf5f1e1210c9ad8051508a55bb9d1e
Roni Shabo <roni.shabo@gmail.com>
2019-02-27 17:55:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
     cheese.include?(cheese_types)
-  string.join
+  return string.join
   end
 end
